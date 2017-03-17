4366f80c90359e48024876c17c7b244be14c02ab
Sumant Bhaskaruni <bsumantb@gmail.com>
2017-03-17 15:25:46 -0400

    Even moar formatting; added comments

--- thathumandude.py
+++ thathumandude.py
@@ -1,74 +1,99 @@
 # -*- coding: utf-8 -*-
 
-from random import *
-from basebot import *
+##################################################
+# thathumandude                                  #
+#                                                #
+# My personal bot for Euphoria. (euphoria.io)    #
+##################################################
 
+from random import *
+from basebot import * # Euphoria bot library
+
+# Sets nick to my inbox nick which all notifies are directed to
 def inbox(match, meta):
 	meta['self'].set_nickname(nick_sans)
 	meta['self'].send_chat('Here you go:', meta['msgid'])
 	meta['self'].set_nickname(bot_nick)
 
-def facepalm(match, meta):
-	if randint(1, 20) == 1:
-		return 'Stop hitting yourself!'
-
+# Prints out start time and the time since
 def uptime(match, meta):
-	started = format_datetime(meta['self'].started, False)
-	since = format_delta(time.time() - meta['self'].started, False)
-	return '/me has been up since %s (%s).' % (str(started), str(since))
+	started = str(format_datetime(meta['self'].started, False))
+	time_since = str(format_delta(time.time() - meta['self'].started, False))
+	
+	return '/me has been up since %s (%s).' % (started, time_since)
 
+# Adds a dict with all the properties of an error
 def add_error(match, meta):
 	errors.append({'sender': meta['sender'], 'message': match.group(1), 'time': time.time()})
-	return 'Thank you for your feedback. Message will be delivered to %s.' % nick_mono
 	
+	return 'Thank you for your feedback. Message will be delivered to %s.' % nick_mono
+
+# Prints out the errors if the sender is me, the creator
 def check_errors(match, meta):
 	if meta['sender'] == nick_mono or meta['sender'] == ':3':
-		for e in range(0, len(errors)):
-			meta['self'].send_chat('[%s, %s ago] %s' % (errors[e]['sender'], str(format_delta(time.time() - errors[e]['time'], False)), errors[e]['message']), meta['msgid'])
-			
-			del errors[e]
+		try:
+			for e in range(0, len(errors) + 1):
+				sender = errors[e]['sender']
+				time_since = str(format_delta(time.time() - errors[e]['time'], False))
+				message = errors[e]['message']
+				
+				meta['self'].send_chat('[%s, %s ago] %s' % (sender, time_since, message), meta['msgid'])
+		except IndexError:
+			pass
+		del errors[:]
 	else:
 		return 'Why do you care? It\'s not your bot. :P'
-	
+
+# Closes the connection, effectively "killing" it
 def kill(match, meta):
 	meta['self'].send_chat('/me is exiting.', meta['msgid'])
 	meta['self'].close()
 
-nick_sans = u'totally\U0001D5C1\U0001D5CE\U0001D5C6\U0001D5BA\U0001D5C7'
-nick_mono = u'totally\U0001D691\U0001D69E\U0001D696\U0001D68A\U0001D697'
-bot_nick = 'thathumandude'
-short_help = '/me is a bot to make pinging %s easier.' % nick_mono
-long_help = 'I am a bot made by and for %s. I make pinging him easier.' % nick_mono
-errors = []
+# Easter egg that has a 1/20 chance od sending "Stop hitting yourself!"
+def facepalm(match, meta):
+	if randint(1, 20) == 1:
+		return 'Stop hitting yourself!'
+
+nick_sans = u'totally\U0001D5C1\U0001D5CE\U0001D5C6\U0001D5BA\U0001D5C7' # My notify nick
+nick_mono = u'totally\U0001D691\U0001D69E\U0001D696\U0001D68A\U0001D697' # My nick
+bot_nick = 'thathumandude' # The bot's nick
+short_help = '/me is a bot to make pinging %s easier.' % nick_mono # The short help message
+long_help = 'I am a bot made by and for %s. I make pinging him easier.' % nick_mono # The long help message
+errors = [] # The array for errors
 
 regexes = {
 	'(?i)^(\s+)?gimme\s+mah\s+nick,?\s+bot!?(\s+)?$':
-		nick_mono,
+		nick_mono, # "gimme mah nick, bot!" returns my nick
 	'(?i)@totallyhuman\b':
-		'@%s' % nick_mono,
+		'@%s' % nick_mono,  # "@totallyhuman" pings my actual nick
 	'(?i)^(\s+)?!THInbox(\s+)?$':
-		inbox,
+		inbox, # "!THInbox" calls the inbox() method
 	'(?i)^(\s+)?/me\s+facepalms(\s+)?$':
-		facepalm,
+		facepalm, # "/me facepalms" calls the facepalm() method
 	'(?i)^(\s+)?!help\s+@?thathumandude(\s+)?$':
-		long_help,
+		long_help, # "!help @thathumandude" returns the long help message
 	'(?i)^(\s+)?!help(\s+)?$':
-		short_help,
+		short_help, # "!help" returns the short help message
 	'(?i)^(\s+)?!ping\s+@?thathumandude(\s+)?$':
-		'Pong!',
+		'Pong!', # "!ping @thathumandude" returns "Pong!"
 	'(?i)^(\s+)?!ping(\s+)?$':
-		'Pong!',
+		'Pong!', # "!ping" returns "Pong!"
 	'(?i)^(\s+)?!creator\s+@?thathumandude(\s+)?$':
-		'/me was created by %s.' % nick_mono,
-	'(?i)^(?:\s+)?!error\s+@?thathumandude(?:\s+)?(.*)$':
-		add_error,
+		'/me was created by %s.' % nick_mono, # "!creator @thathumandude" returns my nick
+	'(?i)^(?:\s+)?!error\s+@?thathumandude(?:\s+)(.*)$':
+		add_error, # "!error @thathumandude" calls the add_error() method
 	'(?i)^(\s+)?!checkerrors\s+@?thathumandude(\s+)?$':
-		check_errors,
+		check_errors, # "!checkerrors @thathumandude" calls the check_errors() method
 	'(?i)^(\s+)?!uptime\s+@?thathumandude(\s+)?$':
-		uptime,
+		uptime, # "!uptime @thathumandude" calls the uptime() method
 	'(?i)^(\s+)?!kill\s+@?thathumandude(\s+)?$':
-		kill
+		kill # "!kill @thathumandude" calls the kill() method
 }
 
 if __name__ == '__main__':
-		run_minibot(botname = bot_nick, nickname = bot_nick, do_uptime = False, regexes = regexes)
+	# Initiates a MiniBot with the following attributes:
+	# botname = bot_nick (nick used for logging)
+	# nickname = bot_nick (actual nick used)
+	# do_uptime = False (disabling the default uptime function)
+	# regexes = regexes (passing the regexes dict for the bot to follow)
+	run_minibot(botname = bot_nick, nickname = bot_nick, do_uptime = False, regexes = regexes)
